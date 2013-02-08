828a8e66f759b9aba586ea372957d384b43f830d
Curtis Lassam <curtis@lassam.net>
2013-02-08 00:06:17 -0800

    Style changes

--- main.js
+++ main.js
@@ -4,34 +4,35 @@ $(document).ready(function() {
     $("#target").focus();
 
     var request_response = function(request){
-	response_callback("Response"); 
+        response_callback("Response"); 
     }
 
     var list_of_options = function(){ 
-	return ["Look at trees.", "Look at other, better trees.", "Fuck you."]; 
+        return ["Look at trees.", "Look at other, better trees.", "Fuck you."]; 
     }
     
     var focus = function(evt) {
-	$("#target").focus(); 
-	return evt;
+        $("#target").focus(); 
+        return evt;
     };
 
     var response_callback = function( response ){
-	$("#history").append( "<li class='computer'> $> "+ response +" </li> " );
-	$("#prompt").show();
+        $("#history").append( "<li class='computer'> $> "+ response +" </li> " );
+        $("#prompt").show();
+        $("#history").scrollTop($("#history")[0].scrollHeight);
     }
 
     var enter = function(evt) {
-	if( $('.typeahead').is(":visible") ){
-	    console.log("typeahead'd");
-	    return evt;
-	}
-	var words = $("#target").val();
-	$("#history").append( "<li class='human'> $> " + words + " </li> " );
-	$("#target").val("");
-	$("#prompt").hide();
-	request_response();
-	return evt;
+        if( $('.typeahead').is(":visible") ){
+            console.log("typeahead'd");
+            return evt;
+        }
+        var words = $("#target").val();
+        $("#history").append( "<li class='human'> $> " + words + " </li> " );
+        $("#target").val("");
+        $("#prompt").hide();
+        request_response();
+        return evt;
     };
 
     $(document).bind('keydown', focus);
